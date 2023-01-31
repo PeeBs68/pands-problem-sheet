5e078cf73f4a77bf118ad67ba7fea7b9c8245228
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-01-31 23:32:53 +0000

    Updated Input Section

--- bank.py
+++ bank.py
@@ -5,6 +5,8 @@
 # input section 
 # NOTE to self - Add validations for number format - should be an INT and not CHAR etc
 # Note to self - allow for negative values
+# Note to self - after input, print value, convert to number with 2 decimal places, print it again as value in Euro
+# Do Likewise with the second input
 
 # calculations
 
