f9a8f16b26c0b08548012e12b0ef425090fbc797
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-01 21:49:59 +0000

    Added Some Notes to Help

--- bank.py
+++ bank.py
@@ -2,7 +2,13 @@
 # Author: Phelim Barry
 # Purpose: Receive two inputs, add them together and output the result
 
+# There is a bit in this, break it down into smaller parts, 
+# for example read in an integer first, (and print it back out again, 
+# then do some arithmetic to it and print, then read in a second one and add the two, 
+# and only then look at the formatting of the answer. of course there are many ways of doing this.
+
 # input section 
+# USE THIS FORMAT num1 = int(input("input Num1 "))
 # NOTE to self - Add validations for number format - should be an INT and not CHAR etc
 # Note to self - allow for negative values
 # Note to self - after input, print value, convert to number with 2 decimal places, print it again as value in Euro
