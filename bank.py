dd891fae167bd70feddca7d2768eec2e6a164e38
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-02 21:17:50 +0000

    Mostly trying ways to ensure 2 deciaml places

--- bank.py
+++ bank.py
@@ -2,18 +2,38 @@
 # Author: Phelim Barry
 # Purpose: Receive two inputs, add them together and output the result
 
+#Links
+# for decimals https://tutorial.eyehunts.com/python/how-to-display-2-decimal-places-in-python-example-code/
+
 # There is a bit in this, break it down into smaller parts, 
 # for example read in an integer first, (and print it back out again, 
 # then do some arithmetic to it and print, then read in a second one and add the two, 
 # and only then look at the formatting of the answer. of course there are many ways of doing this.
 
 # input section 
-# USE THIS FORMAT num1 = int(input("input Num1 "))
+# take the input, print the value as inputed and also converted to euro and cents
 # NOTE to self - Add validations for number format - should be an INT and not CHAR etc
-# Note to self - allow for negative values
-# Note to self - after input, print value, convert to number with 2 decimal places, print it again as value in Euro
-# Do Likewise with the second input
+
+num1 = int(input("Input the first number in cents: "))
+print(f"\nYou entered {num1} cent which is €" "{:.2f}".format(num1/100) + " in Euros and Cents")
+num2 = int(input("Input the second number in cents: "))
+print(f"\nYou entered {num2} cent which is €" "{:.2f}".format(num2/100) + " in Euros and Cents")
+
 
 # calculations
 
+numtotal = (num1 + num2)/100
+
 # output section
+
+print (f"The total is: €{numtotal}")
+
+#to ensure we output 2 decomal places
+newnumtotal = "{:.2f}".format(numtotal)
+
+print (f"The total in Euros and Cents is: €{newnumtotal}")
+# convert to decimal to accurately print 2 decimal places
+from decimal import Decimal
+decimal_numtotal = Decimal("%.2f" % numtotal)
+print ("€",decimal_numtotal)
+#print (f"{newnumtotal:.2f}")
