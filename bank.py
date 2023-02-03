c7ad25a27dcc25a432a640d6e32da12dedc5cd65
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-03 15:35:34 +0000

    Making changes to output formats

--- bank.py
+++ bank.py
@@ -5,9 +5,7 @@
 # Links
 # Information on Decimals taken from the following sources to ensure the output had 2 decimal places 
 # (e.g. 200/100 = 2.0. Needed this to show as 2.00)
-# https://stackoverflow.com/questions/5202233/how-to-change-39-54484700000000-to-39-54-and-using-python
-# https://tutorial.eyehunts.com/python/how-to-display-2-decimal-places-in-python-example-code/ and
-# https://www.pythontutorial.net/advanced-python/python-decimal/
+# https://www.w3schools.com/python/python_string_formatting.asp
 
 # There is a bit in this, break it down into smaller parts, 
 # for example read in an integer first, (and print it back out again, 
@@ -15,23 +13,25 @@
 # and only then look at the formatting of the answer. of course there are many ways of doing this.
 
 # input section 
-# take the input, print the value as inputed and also converted to euro and cents
+# take the input, print the value as inputed and also the value converted to euro and cents
 # NOTE to self - Add validations for number format - should be an INT and not CHAR etc
 
 num1 = int(input("Input the first number in cents: "))
-print(f"\nYou entered {num1} cent which is €" "{:.2f}".format(num1/100) + " in Euros and Cents")
+
+newnum1 = "You entered €{:.2f} euro"
+print(newnum1.format(num1/100))
+
+print(f"\nYou entered {num1} cent which is €" "{:.2f}".format(num1/100) + " Euros")
+
 num2 = int(input("\nInput the second number in cents: "))
-print(f"\nYou entered {num2} cent which is €" "{:.2f}".format(num2/100) + " in Euros and Cents")
+print(f"\nYou entered {num2} cent which is €" "{:.2f}".format(num2/100) + " Euros")
 
 # calculations
-
+# divide the total amount by 100 to convert to euros
 numtotal = (num1 + num2)/100
 
 # output section
-
-# print (f"The total is: €{numtotal}") This would not necessarily print 2 decimal places
-
-#to ensure we output 2 decimal places
+# to ensure we output 2 decimal places
 newnumtotal = "{:.2f}".format(numtotal)
 print (f"\nThe total in Euros and Cents is: €{newnumtotal}")
 
