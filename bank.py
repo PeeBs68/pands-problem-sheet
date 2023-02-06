a79fedd4ba08d28d246a424efae68cb8a86cacd6
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-06 19:38:45 +0000

    Removed unnecessary comments and code

--- bank.py
+++ bank.py
@@ -1,11 +1,10 @@
 # bank.py
 # Author: Phelim Barry
 # Purpose: Receive two inputs in cents, add them together and output the result in euros and cents
-# See readme.md for details on additional sources
+# See readme.md for details on additional sources for example try/except
 
 # input section 
-# take the input, print the value as inputed and also the value converted to euro and cents
-# take the 1st number and validate it is an integer - if not, ask for it to be re-entered
+# take the 1st input and validate it is an integer
 while True:
   try:
     num1 = int(input("\nInput the first number in cents: "))
@@ -14,16 +13,11 @@ while True:
     continue
   break
 
-# above section replaces this simplier line although with no validations
-# num1 = int(input("Input the first number in cents: "))
-
+# output the input value in Cents and Euros
 newnum1 = "€{:.2f} Euro"
-#print(newnum1.format(num1/100))
 print("\nYou Entered",num1,"cent which is",newnum1.format(num1/100))
-# The above 2 lines OR 1 Below do the same thing
-# print(f"\nYou entered {num1} cent which is €" "{:.2f}".format(num1/100) + " Euros")
 
-# this section will input the 2nd number and validate it is an integer - if not the user will be asked to enter it again
+# take the 2nd input and validate it is an integer
 while True:
   try:
     num2 = int(input("\nInput the second number in cents: "))
@@ -31,21 +25,17 @@ while True:
     print('Error - Your number must be an integer!')
     continue
   break
-# above section replaces this line
-# num2 = int(input("\nInput the second number in cents: "))
 
+# output the input value in Cents and Euros
 newnum2 = "€{:.2f} Euro"
-#print(newnum2.format(num2/100))
 print("\nYou Entered",num2,"cent which is",newnum2.format(num2/100))
-# The above 2 lines OR 1 Below do the same thing
-# print(f"\nYou entered {num1} cent which is €" "{:.2f}".format(num1/100) + " Euros")
 
 # calculations
 # add the two numbers and divide the total by 100 to convert to euros
 numtotal = (num1 + num2)/100
 
 # output section
-# define a new variable to hold the total value with 2 decimal places
+# define a new variable to hold the total value and format with 2 decimal places
 newnumtotal = "{0:.2f}".format(numtotal)
 # Add a new line and print the output
 print (f"\nThe total in Euros and Cents is: €{newnumtotal}")
