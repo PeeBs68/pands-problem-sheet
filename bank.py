667ab6e1a0b3068c17798c2250842062a0e5a358
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-07 20:45:57 +0000

    trying to fix something

--- bank.py
+++ bank.py
@@ -5,6 +5,7 @@
 
 # input section 
 # take the 1st input and validate it is an integer
+
 while True:
   try:
     num1 = int(input("\nInput the first number in cents: "))
@@ -13,29 +14,32 @@ while True:
     continue
   break
 
+print (num1)
 # output the input value in Cents and Euros
 newnum1 = "{:.2f} Euro"
-print("\nYou Entered",num1,"cent which is",newnum1.format(num1/100))
+print("\nYou Entered "+newnum1.format(num1/100))
+print("\nYou Entered ",num1,"cent which is",newnum1.format(num1/100))
+print("\nYou Entered ",num1,"cent which is",newnum1.format(num1/100))
 
 # take the 2nd input and validate it is an integer
-while True:
-  try:
-    num2 = int(input("\nInput the second number in cents: "))
-  except ValueError:
-    print('Error - Your number must be an integer!')
-    continue
-  break
+#while True:
+#  try:
+#    num2 = int(input("\nInput the second number in cents: "))
+#  except ValueError:
+#    print('Error - Your number must be an integer!')
+#    continue
+#  break
 
 # output the input value in Cents and Euros
-newnum2 = "{:.2f} Euro"
-print("\nYou Entered",num2,"cent which is",newnum2.format(num2/100))
+#newnum2 = "{:.2f} Euro"
+#print("\nYou Entered",num2,"cent which is",newnum2.format(num2/100))
 
 # calculations
 # add the two numbers and divide the total by 100 to convert to euros
-numtotal = (num1 + num2)/100
+#numtotal = (num1 + num2)/100
 
 # output section
 # define a new variable to hold the total value and format with 2 decimal places
-newnumtotal = "{:.2f}".format(numtotal)
+#newnumtotal = "{:.2f}".format(numtotal)
 # Add a new line and print the output
-print (f"\nThe total in Euros and Cents is: {newnumtotal}")
+#print (f"\nThe total in Euros and Cents is: {newnumtotal}")
