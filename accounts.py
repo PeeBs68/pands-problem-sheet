7efe401ec01faef850738af102f0fbe612605d64
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-08 23:23:21 +0000

    Initial placeholder for accounts.py

---
+++ accounts.py
@@ -0,0 +1,16 @@
+# accounts.py
+# Author: Phelim Barry
+# Purpose: read in a 10 character account number and output the 
+# number with the last 4 digits showing as input and the others replaced with XXXXXX
+# Do likewise with a string of any length
+
+# Part1
+# read in the account number as a string (may contain letters and numbers) with 10 characters
+# replace the 1st 6 characters with an 'X'
+# print the result
+
+# Part2
+# read in the account number as a string (may contain letters and numbers)
+# check the length with len
+# replace the 1st (len-4) characters with an 'X'
+# print the result
