7d3cbe8df86b6b7b07360db6666da644be557164
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-13 01:39:08 +0000

    Added new comments

--- accounts.py
+++ accounts.py
@@ -6,22 +6,24 @@
 
 # Part1: read in the 10 digit account number as a string
 acc_num = input("Please enter the 10 digit account number: ")
-# strip out the last 4 digits to be used in the result
+# strip out and store the last 4 digits to be used in the result
 last_4_digits = acc_num[6:]
-# Simplistic way of formatting the first 6 characters with an X. 
-# Note: Only works if the account number is in fact 10 characters
+# Note: This is a simplistic way of replacing the first 6 characters with an X. 
+# Note: And only works if the account number is in fact 10 characters
+# Add the X's to the last 4 digits
 acc_num_masked = 6*"X"+last_4_digits
+# Print the result
 print (f"\nThe account number in masked format is {acc_num_masked}")
 
 # Part2: read in the account number, which could be any length
 # Note: if 4 or less characters are entered then the code will output the number without changes
 acc_num2 = input("Please enter the account number (of any length): ")
-# check the length
+# get the length of the account number
 acc_num2_len = len(acc_num2)
-# amount of account number to be masked i.e. length-4 
+# get the amount of the account number needing to be masked i.e. length-4 
 first_part = (acc_num2_len-4)*"X"
 # strip out the last 4 digits into a new variable to be used in the output
 last_4_digits2 = acc_num2[-4:]
-# Combine part to be masked with the last 4 digits and print the result
+# Combine the part to be masked with the last 4 digits and print the result
 acc_num2_masked = first_part+last_4_digits2
 print (f"\nThe account number in masked format is {acc_num2_masked}")
