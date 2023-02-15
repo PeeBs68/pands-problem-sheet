9dfd86f7e15e79771640dd22c5d7de424e011ce7
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-15 23:43:15 +0000

    Initial work on collatz.py

--- collatz.py
+++ collatz.py
@@ -1,3 +1,15 @@
 # collatz.py
 # Author: Phelim Barry
 # Purpose: <placeholder for now>
+
+# Input a number
+number = int(input("Enter a positive number: "))
+# While number is not equal to 1 do the below until it is
+while (number !=1):
+    print (number) # NOTE Seems to get stuck on this...
+if (number % 2) == 0:
+        newnum=(number/2)
+else:
+        newnum=(number*3)+1
+print (number, newnum)
+
