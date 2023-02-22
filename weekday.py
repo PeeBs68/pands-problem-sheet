e68840de4dcec406d2610dd659b83ef61ada5a76
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-22 16:21:19 +0000

    Placeholder for weekday.py

---
+++ weekday.py
@@ -0,0 +1,9 @@
+# weekday.py
+# Author: Phelim Barry
+# Purpose:  outputs whether today is a weekday or not
+
+# Needs some googling to figure out what day today is
+
+
+# Output1: Yes, unfortunately today is a weekday
+# Output2: It is the weekend, yay!
