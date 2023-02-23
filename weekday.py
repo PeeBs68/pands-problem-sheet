872dfacb8c9df262eb6643e654e60a1180032866
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-23 00:56:18 +0000

    Removed unnecessary comments

--- weekday.py
+++ weekday.py
@@ -1,37 +1,16 @@
 # weekday.py
 # Author: Phelim Barry
-# Purpose:  outputs whether today is a weekday or not
+# Purpose: outputs whether today is a weekday or not
 
-# Needs some googling to figure out what day today is
-# Need to import datetime maybe
-# then use date.weekday() which will produce an int value 
-
-# Output1: print ("Yes, unfortunately today is a weekday")
-# Output2: print ("It is the weekend, yay!")
-#For assignment5 - day of the week
-# Getting the day - NOTE Monday = 1, Tuesday = 2, etc
+# Need to import the datetime module
 import datetime
-#now = datetime.datetime.now()
-#print (now)
-#today = datetime.datetime.today()
-#print (today)
+# Create a list of days 
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 #from https://stackoverflow.com/questions/9847213/how-do-i-get-the-day-of-week-given-a-date
+# Returns a numeric value for the day: 0 = Monday, 2 = Tuesday etc.
 day = datetime.datetime.today().weekday()
-print (day) # use this one
-#now, 2 options to know what to print out
-#1 create a list ("monday", "Tuesday" etc)
-# then match the day with the position in the list
-days = ["Monday", "Tuesday", "Wednesday", "thursday", "Friday", "Saturday", "Sunday"]
-# print (days[1])
-#Or if day number between 0 and 4 then its a weekday else is a weekend
-if day >=0 and day <5:
-    print (f"Today is a weekday, it's only {days[day]}")
-else:
+# Check the number and print out it's equivilent from list of days
+if day >=0 and day <5: # It's a weekday
+    print (f"Today is a weekday, it's only {days[day]} unfortunately!")
+else: # If not a week day then it must be the weekend
     print (f"Today is {days[day]} so its the weekend!!!")
-#print (f"Today is: {days[day]}")
-
-# This is very short, maybe add the actual day from the list as well - something like
-#if day >=0 and day <5:
-#    print (f"Today is a weekday - it's only {days[day]}")
-#else:
-#    print ("Its the weekend")
