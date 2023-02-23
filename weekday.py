72f03e721cbfb6efdc5d5d8996cf7b41ee7d33c7
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-23 10:12:29 +0000

    Fixed mistake with day numbering

--- weekday.py
+++ weekday.py
@@ -7,8 +7,9 @@ import datetime
 # Create a list of days 
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 #from https://stackoverflow.com/questions/9847213/how-do-i-get-the-day-of-week-given-a-date
-# Returns a numeric value for the day: 0 = Monday, 2 = Tuesday etc.
+# Returns a numeric value for the day: 0 = Monday, 1 = Tuesday etc.
 day = datetime.datetime.today().weekday()
+print (day)
 # Check the number and print out it's equivilent from list of days
 if day >=0 and day <5: # It's a weekday
     print (f"Today is a weekday, it's only {days[day]} unfortunately!")
