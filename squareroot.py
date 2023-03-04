a0ee421d45b60107f44569e2ff622ba1e115f42a
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-04 21:00:59 +0000

    Initial thoughts and notes

--- squareroot.py
+++ squareroot.py
@@ -3,11 +3,15 @@
 # Purpose: script to use a function to calculate the square root of a positive floating point number
 
 # look at the newton method at estimating square roots.
+# Minute 17 of this vid has some ideas - https://www.youtube.com/watch?v=szQUIRPrAgQ
+
 
 def sqrt():
 
 #Sample Input
 num = input("Enter a positive number: ")
+
+sqrt(num)
 
 #Sample output
 print(f"The square root of {num} is {newnum}")
