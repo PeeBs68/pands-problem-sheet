469fc3d02b6bbb955d7d2335f813d333f9fe2818
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-04 20:31:09 +0000

    initial file for task06

---
+++ squareroot.py
@@ -0,0 +1,13 @@
+# squareroot.py
+# Author: Phelim Barry
+# Purpose: script to use a function to calculate the square root of a positive floating point number
+
+# look at the newton method at estimating square roots.
+
+def sqrt():
+
+#Sample Input
+num = input("Enter a positive number: ")
+
+#Sample output
+print(f"The square root of {num} is {newnum}")
