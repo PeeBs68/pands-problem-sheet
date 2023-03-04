179a8509172fc7bee7f3b8131cbf6e715a638d87
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-04 23:34:33 +0000

    Added some initial flow ideas

--- squareroot.py
+++ squareroot.py
@@ -5,13 +5,24 @@
 # look at the newton method at estimating square roots.
 # Minute 17 of this vid has some ideas - https://www.youtube.com/watch?v=szQUIRPrAgQ
 
-
 def sqrt():
 
 #Sample Input
 num = input("Enter a positive number: ")
+# Do a check here to see if it is positive
 
-sqrt(num)
+#Define the function and input values
+#Input Number, Number of iterations, Tolerence Level,
+sqrt(num,num_of_iterations,tolerence_level)
+    for x in range (num_of_iterations):
+    #formula here...x= something
+        if abs(x) is > tolerence_level:
+            x = newx
+        else
+            print result
+    
+num_of_iterations = 100
+tolerence_level = .001 
 
 #Sample output
 print(f"The square root of {num} is {newnum}")
