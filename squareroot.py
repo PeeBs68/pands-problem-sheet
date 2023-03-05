1b4fe5c768822b501dfd56e754ae60fdb84aef5e
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-05 20:28:42 +0000

    Added some more bits for testing

--- squareroot.py
+++ squareroot.py
@@ -8,11 +8,13 @@ def sqrt(number, num_of_iterations, tolerence_level):
     counter=0
     #for x in range (num_of_iterations):
     while counter < num_of_iterations:
+        print (counter)
         #formula here...x= something
         newnumber = number+1
         print (number)
         counter+=1
         number=number+1
+        print (number)
         #while abs(x) is > tolerence_level:
         return newnumber
 
