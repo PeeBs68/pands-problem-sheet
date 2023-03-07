31f56f9ff3e81f9e20571a5c1ac9d5db64abedb7
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-07 21:12:30 +0000

    added some comments and links for reference

--- squareroot.py
+++ squareroot.py
@@ -4,11 +4,15 @@
 # look at the newton method at estimating square roots.
 # Minute 17 of this vid has some ideas - https://www.youtube.com/watch?v=szQUIRPrAgQ
 # Formula here https://hackernoon.com/calculating-the-square-root-of-a-number-using-the-newton-raphson-method-a-how-to-guide-yr4e32zo
+# Youtube vid showing the maths in plain English - https://www.youtube.com/watch?v=2158QbsunA8
+
+#NOTE add in actual maths formula and iterative formula (as below)
 
 def sqrt(num):
         tempnum = num
+        #use abs to get a positive value
         while abs(tempnum - num * num) > tolerence_level:
-#                print (abs(tempnum - num * num))
+                #get a new guess and try again
                 num = (num+(tempnum/num))/2
                 print (num)
                 root = num
