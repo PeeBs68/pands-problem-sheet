77790516a4915e4acef48687368452533a515e1b
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-08 22:02:41 +0000

    Added placeholder for es.py

---
+++ es.py
@@ -0,0 +1,7 @@
+# es.py
+# Author: Phelim Barry
+# Purpose: Write a program that reads in a text file and outputs the number of e's it contains
+
+# Get the file name ...The program should take the filename from an argument on the command line. I have not shown you how to do this, you need to look it up
+
+#Print out the number of e's
