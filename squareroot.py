b980653b77f7279ae89558987ab2eb7fb532c550
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-08 21:13:24 +0000

    Removed old references and transferred them to the Readme

--- squareroot.py
+++ squareroot.py
@@ -3,15 +3,13 @@
 # Purpose: script to use a function to calculate the square root of a positive floating point number
 # Using the newton method of estimating square roots.
 
-#NOTE add in actual maths formula and iterative formula (as below)
-
+#define the function
 def sqrt(num):
         #define orig_num (as the original number), because we don't want that to change
         orig_num = num
 
         #Try a first guess and see the answer is less than the tolerence we set below (will usually be way off)
-        #use abs to get a positive value
-
+        #use abs to use a positive value
         while abs(orig_num - num * num) > tolerence_level:
                 #get a new guess and try again
                 num = (num+(orig_num/num))/2
@@ -21,9 +19,8 @@ def sqrt(num):
         return root
 
 # the actual code
-#We can set these two if we want - need to use one or the other (or both)
-num_of_iterations = 10 #how many times we want the while loop to run
-tolerence_level = .000001 #how close we want the resut to be 
+#set a tolerance level - i.e. how accurate do we want the result to be
+tolerence_level = .000001
 
 #define input num as a float so decimals can be entered
 num = float(input("Please enter a positive number:  "))
@@ -32,7 +29,7 @@ num = float(input("Please enter a positive number:  "))
 while num < 0:
         num = float(input("Please try again:  "))
 
-#Call the funtion with additional arguements if need be
+#Call the funtion with arguement num
 root = sqrt(num)
 
 #print out the result to 1 decimal place
