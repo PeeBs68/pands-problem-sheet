4aa897f49eaa4140f5918741bc5ee2de8f184a06
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-09 23:54:14 +0000

    added a new reference for command line arguements

--- es.py
+++ es.py
@@ -3,6 +3,7 @@
 # Purpose: Write a program that reads in a text file and outputs the number of e's it contains
 
 # Get the file name ...The program should take the filename from an argument on the command line. I have not shown you how to do this, you need to look it up
+# Maybe here - https://www.geeksforgeeks.org/command-line-arguments-in-python/
 # use this link - https://www.w3schools.com/python/python_file_open.asp
 # this will also be useful - https://realpython.com/read-write-files-python/
 
