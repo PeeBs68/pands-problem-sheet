145a6ae9762cab8d56b3a1e4b2a1c051c0c9356f
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-09 01:42:31 +0000

    Added a few links for es.py

--- es.py
+++ es.py
@@ -3,5 +3,7 @@
 # Purpose: Write a program that reads in a text file and outputs the number of e's it contains
 
 # Get the file name ...The program should take the filename from an argument on the command line. I have not shown you how to do this, you need to look it up
+# use this link - https://www.w3schools.com/python/python_file_open.asp
+# this will also be useful - https://realpython.com/read-write-files-python/
 
 #Print out the number of e's
