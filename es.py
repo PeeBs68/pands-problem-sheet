4d3cbe0dbb67527256fe08a4df2745c768f89039
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-10 02:16:17 +0000

    added some notes for the solution

--- es.py
+++ es.py
@@ -7,4 +7,10 @@
 # use this link - https://www.w3schools.com/python/python_file_open.asp
 # this will also be useful - https://realpython.com/read-write-files-python/
 
+'''To make that possible sys method was imported. Using the sys.argv[1] variable, it is defined that the filename is second argument when calling a program ( sys.argv[0] is the program we are trying to start ).
+References for this part of program go to Python documentation and Geeksforgeeks.org.
+
+With the open( filename,'r' ) function we are opening a file that we called in the command line argument, and making it available just for reading. For counting the lower case letter 'e' the method count() was used, and the argument is a string "e". Reference for the count() method is Programiz.
+'''
+
 #Print out the number of e's
