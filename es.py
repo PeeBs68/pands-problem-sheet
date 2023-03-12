47e023fa15b8216c071676b621007a5822d1ba15
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-12 00:17:08 +0000

    Added some comments and references

--- es.py
+++ es.py
@@ -8,16 +8,18 @@
 #Import the sys module
 import sys
 
-#get the filename from the command line
+#get the filename from the command line - argv[0] would be the actual py file, argv[1] is the filename
 FILENAME = sys.argv[1]
 
 #Note this will be an exact search - i.e. lowercase e only
 letter_to_find = "r"
+#Open the file in read mode and read in the contents as a string
+
 with open(FILENAME, 'r') as f:
         string1 = f.read()
 
-#count the number of e's (or whatever the variable letter_to_fine is set to)
-num_of_e = string1.count(letter_to_find)
-print (f"The number of {letter_to_find}'s in {FILENAME} is {num_of_e}")
+#count the number of e's (or whatever the variable letter_to_find is set to)
+var1 = string1.count(letter_to_find)
+print (f"The number of {letter_to_find}'s in {FILENAME} is {var1}")
 
-#https://www.geeksforgeeks.org/command-line-arguments-in-python/
+#https://www.geeksforgeeks.org/command-line-arguments-in-python/ - for the command line arguements
