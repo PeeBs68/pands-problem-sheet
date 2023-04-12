7880c7024ed9a07171a8e6da680d26004a25e996
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-04-12 21:24:46 +0100

    Updated to accomodate large files by reading the file line by line

--- es.py
+++ es.py
@@ -2,23 +2,24 @@
 # Author: Phelim Barry
 # Purpose: Write a program that reads in a text file and outputs the number of e's it contains
 
-#Import the sys module
+#Import the sys module, used for command line arguements
 import sys
 
-#get the filename from the command line - argv[1] contains the 1st arguement passed to the script (i.e. filename)
+#Get the filename from the command line - argv[1] contains the 1st arguement passed to the script (i.e. filename)
 FILENAME = sys.argv[1]
 
-#define the letter we want to find and count
+#Define the letter we want to find and count
 letter_to_find = "e"
 
-#Open the file in read mode and read in the contents as a string
-with open(FILENAME, 'r') as f:
-        string1 = f.read()
+#Initialise a counter
+counter=0
 
-#count the number of e's (or whatever the variable letter_to_find is set to) and print the result
-var1 = string1.count(letter_to_find)
-print (f"The number of {letter_to_find}'s in {FILENAME} is {var1}")
+#Open the file in read mode
+with open(FILENAME) as r:
+    #Read in the contents line by line and update the counter with the number of occurances
+    for line in r:
+        counter = counter+line.count(letter_to_find)
 
-#https://www.geeksforgeeks.org/command-line-arguments-in-python/ - for the command line arguements
-# use this link - https://www.w3schools.com/python/python_file_open.asp
-# this will also be useful - https://realpython.com/read-write-files-python/
+#Print the final counter value
+print (f"The number of {letter_to_find}'s in {FILENAME} is {counter}")
+
